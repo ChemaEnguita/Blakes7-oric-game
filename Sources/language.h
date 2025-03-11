1dfd0f472df1f03a9906c0e28a2701b91380f8b7
Chema Enguita <enguita@gmail.com>
2025-03-11 20:51:19 +0100

    Version 1.4

---
+++ Sources/language.h
@@ -0,0 +1,16 @@
+/*;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;   
+;; ------------------------------------------    
+;;                 OASIS                         
+;; Oric Adventure Script Interpreting System     
+;; ------------------------------------------    
+;;             (c) Chema 2015                    
+;;            enguita@gmail.com                  
+;; ------------------------------------------    
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;*/  
+ 
+/* Note: this file is generated dynamically  */  
+/*       from the osdk_build.bat script      */  
+ 
+/* Target Language for game localization:    */  
+#define SPANISH 
+ 
